8df516b2479bbb8ced09fcb4bc5974a0d952b1da
obsideen <97191699+Pritthish20@users.noreply.github.com>
2024-02-29 19:40:45 +0530

    Add files via upload

---
+++ login & signin page/signin.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    const toggle1 = document.getElementById("box1");
+    const toggle2 = document.getElementById("box2");
+    toggle1.addEventListener("click", () => {
+      toggle1.style.animation = "none";
+    });
+    toggle2.addEventListener("click", () => {
+      toggle1.style.animation = "none";
+    });
+  };
+  
