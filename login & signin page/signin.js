8b752f4094c62c078cadcf7fbbbca01e96bda185
obsideen <97191699+Pritthish20@users.noreply.github.com>
2024-02-29 19:39:23 +0530

    Delete login & signin page directory

--- login & signin page/signin.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    const toggle1 = document.getElementById("box1");
-    toggle1.addEventListener("click", () => {
-      toggle1.style.animation = "none";
-    });
-  };
-  
